51fdd69fb6856de9b44e0a9a640a955d18ffb444
Oliver Holst <oliver.holst@sonova.com>
2023-06-29 14:33:25 +0200

    removed config.cpp from .gitignore

---
+++ src/config.cpp
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// mqtt configuration (username, password not used)
+const char *  mqtt_server = "cloud.tbz.ch";
+const char *  clientId = "oliverholst";
+const char *  username = "";
+const char *  password = "";
+
+// WiFi configuration
+const char * ssid = "LERNKUBE";
+const char * passphrase = "l3rnk4b3";
+
+#endif // BASE_H
